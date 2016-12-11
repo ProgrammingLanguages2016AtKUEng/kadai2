fbc99feb06a33d3212576ac70ccbe11372909f6a
igarashi <igarashi@kuis.kyoto-u.ac.jp>
2016-12-11 17:38:48 +0900

    Initial commit.

---
+++ C/tree.c
@@ -0,0 +1,31 @@
+#include <stdio.h>   // for printf
+#include <stdlib.h>  // for malloc
+#include <stdbool.h> // for type bool, true, and false
+#include <assert.h>  // for assert
+
+struct tree {
+  /* FILL IN HERE */
+};
+
+int value(struct tree *t) {
+  /* FILL IN HERE */
+}
+
+// kadai 2-2
+// void print_tree(struct tree *t) {
+//   /* FILL IN HERE */
+// }
+
+// kadai 2-3
+// void print_tree_fewer_parens(struct tree *t) {
+//   /* FILL IN HERE */
+// }
+
+// kadai 2-4
+// struct tree *reduce(struct tree *t) {
+//   /* FILL IN HERE */
+// }
+
+int main(void) {
+  /* FILL IN HERE */
+}
